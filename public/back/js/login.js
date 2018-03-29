1149790f24c251f66127d5258b1c1ce03db6b547
Jepson <jepsonpp@qq.com>
2018-03-29 17:16:56 +0800

    登录模块: 登录错误校验提示完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -29,8 +29,11 @@ $(function() {
             min: 2,
             max: 6,
             message: "用户名长度必须是2-6位"
+          },
+          // callback 专门用于提示
+          callback: {
+            message: "用户名不存在"
           }
-          
         }
       },
       // 校验密码
@@ -44,6 +47,10 @@ $(function() {
             min: 6,
             max: 12,
             message: "密码长度必须是6-12位"
+          },
+          // callback 专门用于提示
+          callback: {
+            message: "密码错误"
           }
           
         }
@@ -75,13 +82,16 @@ $(function() {
       data: $("#form").serialize(),
       dataType: "json",
       success: function( data ) {
-        console.log(data);
+        // console.log(data);
         if ( data.error === 1000 ) {
-          alert( "用户名错误" );
+          // alert( "用户名错误" );
+          // updateStatus( 校验字段, 状态, 哪个规则的message )
+          $("#form").data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
         }
         
         if ( data.error === 1001 ) {
-          alert( "密码错误" );
+          // alert( "密码错误" );
+          $("#form").data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
         }
         
         if ( data.success ) {
