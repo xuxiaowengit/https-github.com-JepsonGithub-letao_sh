2fed779417283a43c696a7e9b5ebc0ca898d4762
Jepson <jepsonpp@qq.com>
2018-03-29 17:49:31 +0800

    全局ajax进度条功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,21 @@
 /**
  * Created by Jepson on 2018/3/29.
  */
+
+// 禁用小环环
+NProgress.configure({ showSpinner: false });
+
+// ajax 开始
+$( document ).ajaxStart(function() {
+  // 进度条加载效果
+  NProgress.start();
+});
+
+// ajax 结束
+$( document ).ajaxStop( function() {
+  // 模拟网络延迟
+  setTimeout(function() {
+    // 进度条关闭
+    NProgress.done();
+  }, 500);
+})
