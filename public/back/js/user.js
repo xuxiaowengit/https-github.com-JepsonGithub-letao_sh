e4f45d1820ef68003ff3de9cca99b6de9e873bf6
Jepson <jepsonpp@qq.com>
2018-03-31 00:08:43 +0800

    用户管理: 分页渲染

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -5,23 +5,42 @@
 $(function() {
 
   // 发送 ajax 请求, 获取用户数据, 渲染到页面中
-  var page = 1;
+  var currentPage = 1;
   var pageSize = 5;
+  
+  // 页面一加载, 就渲染一次
+  render();
+  function render() {
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      success: function ( data ) {
+        console.log( data );
+        // template( "模板id", "数据对象" );
+        // 在模板中可以直接使用数据对象中的所有属性
+        var htmlStr = template("userTpl", data);
+        $('.main_body tbody').html( htmlStr );
+      
+      
+        // 渲染分页
+        $('#paginator').bootstrapPaginator({
+          // bootstrap使用的版本
+          bootstrapMajorVersion: 3,
+          currentPage: data.page, //当前页
+          totalPages: Math.ceil( data.total / data.size ), //总页数
+          size: "small",
+          onPageClicked:function(a, b, c, page){
+            currentPage = page;
+            render();
+          }
+        })
+      }
+    });
+  }
 
-  $.ajax({
-    type: "get",
-    url: "/user/queryUser",
-    data: {
-      page: page,
-      pageSize: pageSize
-    },
-    success: function ( data ) {
-      console.log( data );
-      // template( "模板id", "数据对象" );
-      // 在模板中可以直接使用数据对象中的所有属性
-      var htmlStr = template("userTpl", data);
-      $('.main_body tbody').html( htmlStr );
-    }
-  })
   
 });
