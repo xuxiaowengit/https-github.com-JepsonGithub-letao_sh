5156d81cc730afb585f544fac8cf1c34d0771226
Jepson <jepsonpp@qq.com>
2018-03-30 21:42:15 +0800

    首页模块: 登录退出功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -20,15 +20,53 @@ $( document ).ajaxStop( function() {
   }, 500);
 });
 
-
 // 二级分类显示隐藏效果
 $('.aside_category').click(function() {
   $(this).next().stop().slideToggle();
 });
-
 
 // 菜单栏隐藏效果
 $('.icon_menu').click(function() {
   $('.aside').toggleClass("menuchange");
   $(".main").toggleClass("menuchange");
 });
+
+// 显示退出模态框
+$(".icon_logout").click(function() {
+  $('#logoutModal').modal();
+});
+
+// 退出按钮退出功能
+$('#logoutBtn').click(function() {
+  // 发送退出 ajax 请求
+  $.ajax({
+    type: "get",
+    url: "/employee/employeeLogout",
+    success: function( data ) {
+      console.log( data )
+      if ( data.success ) {
+        // 退出成功
+        location.href = "login.html";
+      }
+    }
+  })
+});
+
+
+// 登录页面不用判断
+if ( location.href.indexOf("login.html") == -1 ) {
+  // 查看用户是否登录
+  $.ajax({
+    type: "get",
+    url: "/employee/checkRootLogin",
+    success: function( data ) {
+      if ( data.success ) {
+        // 登录了, 啥也不用干
+      }
+      if ( data.error === 400 ) {
+        // 没登陆
+        location.href = "login.html";
+      }
+    }
+  });
+}
