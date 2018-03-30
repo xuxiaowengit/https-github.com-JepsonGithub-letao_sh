19b3b994c9c8a2c14210594085e21bf36bc98884
Jepson <jepsonpp@qq.com>
2018-03-30 23:47:17 +0800

    用户管理: 数据渲染

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,3 +1,27 @@
 /**
  * Created by Jepson on 2018/3/30.
  */
+
+$(function() {
+
+  // 发送 ajax 请求, 获取用户数据, 渲染到页面中
+  var page = 1;
+  var pageSize = 5;
+
+  $.ajax({
+    type: "get",
+    url: "/user/queryUser",
+    data: {
+      page: page,
+      pageSize: pageSize
+    },
+    success: function ( data ) {
+      console.log( data );
+      // template( "模板id", "数据对象" );
+      // 在模板中可以直接使用数据对象中的所有属性
+      var htmlStr = template("userTpl", data);
+      $('.main_body tbody').html( htmlStr );
+    }
+  })
+  
+});
