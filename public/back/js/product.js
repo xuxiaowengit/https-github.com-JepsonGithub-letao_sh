5d087423641e4e66df3691ff324a2cc9d24b61f2
Jepson <jepsonpp@qq.com>
2018-04-01 02:08:24 +0800

    商品管理: 添加功能完成

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -5,7 +5,7 @@
 $(function() {
 
   var currentPage = 1;
-  var pageSize = 2;
+  var pageSize = 5;
   var imgArr = [];
   
   render();
@@ -90,7 +90,6 @@ $(function() {
     });
   });
   
-  
   // 2. 通过委托给 a 设置点击事件
   $('#dropdownMenu').on("click", "a", function() {
     // 获取 id
@@ -100,6 +99,10 @@ $(function() {
     $("#dropdownText").text( selectText );
     // 设置 id 给隐藏域
     $('[name="brandId"]').val(id);
+    
+    // 重置校验状态为 VALID
+    // updateStatus( "字段", "状态", "规则" )
+    $('#form').data("bootstrapValidator").updateStatus("brandId", "VALID")
   });
   
   // 3. 图片上传
@@ -117,13 +120,144 @@ $(function() {
         // 删除最老的图片, 找到最后一个 img 元素, 自杀
         $('#imgBox img:last-of-type').remove();
       }
-      
       // 将上传的图片, 渲染到页面中
       $("#imgBox").prepend('<img src="' + imgObj.picAddr + '" width="100" height="100" alt="">')
       imgArr.push( imgObj );
-  
-      console.log(imgArr);
+      
+      
+      if ( imgArr.length === 3 ) {
+        // 说明已经上传 3 张图片了
+        $('#form').data("bootstrapValidator").updateStatus("picFlag", "VALID");
+      }
     }
+  });
+  
+  // 4. 表单校验
+  $('#form').bootstrapValidator({
+    // 重置排除项
+    excluded: [],
+  
+    // 指定校验小图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    
+    // 指定校验字段
+    fields: {
+      // 二级分类 id
+      brandId: {
+        validators: {
+          notEmpty: {
+            message: "请选择二级分类"
+          }
+        }
+      },
+      proName: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品名称"
+          }
+        }
+      },
+      proDesc: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品描述"
+          }
+        }
+      },
+      // 库存, 只能是数字且不能以 0 开头
+      num: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品库存"
+          },
+          // regexp 正则校验
+          regexp: {
+            // 正则规则
+            regexp: /^[1-9]\d*$/,
+            message: "请输入有效的商品库存"
+          }
+        }
+      },
+      // 尺码, 32-46
+      size: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品尺码"
+          },
+          // regexp 正则校验
+          regexp: {
+            // 正则规则
+            regexp: /^\d{2}-\d{2}$/,
+            message: "请输入一个合法的尺码, 例如 32-44 "
+          }
+        }
+      },
+      oldPrice: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品原价"
+          }
+        }
+      },
+      price: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品现价"
+          }
+        }
+      },
+      // 校验是否上传满三张图片了
+      picFlag: {
+        validators: {
+          notEmpty: {
+            message: "请上传 3 张图片"
+          }
+        }
+      }
+    }
+    
+  });
+  
+  
+  // 5. 验证通过, 阻止默认提交
+  $("#form").on("success.form.bv", function( e ) {
+    e.preventDefault();
+    // 还需要 picName1 picAddr1 picName2 picAddr2 picName3 picAddr3
+
+    var paramsStr = $('#form').serialize();
+    paramsStr += "&picName1=" + imgArr[0].picName + "&picAddr1=" + imgArr[0].picAddr;
+    paramsStr += "&picName2=" + imgArr[1].picName + "&picAddr2=" + imgArr[1].picAddr;
+    paramsStr += "&picName3=" + imgArr[2].picName + "&picAddr3=" + imgArr[2].picAddr;
+    
+    $.ajax({
+      type: "post",
+      url: "/product/addProduct",
+      data: paramsStr,
+      success: function( data ) {
+        console.log(data);
+        if ( data.success ) {
+          // 添加成功
+          // 1. 关闭模态框
+          $('#addProductModal').modal("hide");
+          
+          // 2. 重新渲染第一个页面
+          currentPage = 1;
+          render();
+          
+          // 3. 重置表单内容, 不仅校验重置, 内容也重置
+          $("#form").data("bootstrapValidator").resetForm(true);
+          // 4. 重置下拉框文本
+          $('#dropdownText').text("请选择二级分类");
+          // 5. 删除所有图片
+          $('#imgBox img').remove();
+        }
+      }
+    })
+    
   })
   
 })
