d0f26e5a4000160db9601312d49a4284ee18ff95
Jepson <jepsonpp@qq.com>
2018-04-03 01:58:21 +0800

    mobile会员中心完成

---
+++ public/front/js/user.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Jepson on 2018/4/3.
+ */
+$(function () {
+  
+  //需要发送ajax请求，获取到用户的信息
+  $.ajax({
+    type:"get",
+    url:"/user/queryUserMessage",
+    success:function (info) {
+      
+      if(info.error=== 400){
+        location.href = "login.html";
+      }
+      
+      //直接渲染用户信息
+      console.log(info);
+      $(".userinfo").html( template("tpl", info) );
+      
+      
+    }
+  });
+  
+  
+  
+  //退出功能
+  $(".btn_logout").on("click", function () {
+    
+    $.ajax({
+      type:"get",
+      url:"/user/logout",
+      success:function (info) {
+        if(info.success) {
+          location.href = "login.html";
+        }
+      }
+    });
+    
+  })
+});
